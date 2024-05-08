9e2c91f2af1bf53753c11024818cb923568c19b0
SaraHadou <s.hadou.user@gmail.com>
2024-05-08 18:51:06 +0300

    solve merge two sorted lists problem.

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,57 @@
+// You are given the heads of two sorted linked lists list1 and list2.
+//
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+//
+// Return the head of the merged linked list.
+//
+// Example:
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+
+}
+
+
+class  MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+        System.out.println(mergeTwoSortedLists.mergeTwoLists(list1, list2));
+    }
+
+}
